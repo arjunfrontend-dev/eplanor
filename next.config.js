37afafe46b5c66f1c84e59988096b2b9a1b4f3f0
arjunfrontend-dev <arjunfrontend@gmail.com>
2025-08-27 13:58:00 +0530

    Add GitHub Actions for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-   basePath: '',
+  output: "export",
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
